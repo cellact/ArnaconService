502ad7e78841706f7b9fa2f3bb26dd2013483306
Matan-Fridman <matan@cellact.com>
2025-07-18 15:31:43 +0200

    updated contracts

--- amoy.js
+++ amoy.js
@@ -1,23 +1,24 @@
 const amoy = {
     rpcUrl: 'https://rpc-amoy.polygon.technology/',
     contractAddresses: {
-        "ENSRegistry": "0xb32Feb0530C73FEc90BB985fA40894196938a9f6",
-        "BaseRegistrarImplementation": "0x42E43F785A42bB1696c0a1A036cDe8c7d00d4f4C",
-        "ReverseRegistrar": "0x85FBec85C99b3A8d236e5c64e861f7E62Cdf2De8",
-        "StaticMetadataService": "0xADA2302c6F40269d6DcCd038F8b8B3c2FAe60dd3",
-        "NameWrapper": "0xeb20750987C515732AE4741946f6def43a0200Dd",
-        "PublicResolver": "0xB579f4c45e0CC62f2b4b3A75F23D883E6474A26B",
-        "SimplePriceOracle": "0x78b003C0d809F8e3FC57d8f37Ae2918dbc998512",
-        "ETHRegistrarController": "0x35D74fB7Dc2eA96730Eec42CDe06585D3b17D4c3",
-        "poseidon-solidity/PoseidonT3.sol:PoseidonT3": "0xF58959e3F862557397122cc2809df276b4C83352",
-        "SemaphoreVerifier": "0x6C42599435B82121794D835263C846384869502d",
-        "GlobalRegistrarController": "0xe6cCD9d4dBbb37412E34B75C6B5aD0425875Ecb6",
-        "SecondLevelController": "0xfe86D999605f9A2B65E9a3537Cea070a7aA5C4c0",
-        "SecondLevelInteractor": "0xB09c92078B355F911F25087dd1803e55adC610d4",
-        "ProductTypeRegistry": "0x75A2CD3B7F2248C81911Df41738b476c9083a31A",
-        "ArnaconResolver": "0x36b579adeaD9979eC10200455481B1F44872BaC3",
-        "ProductsNFT": "0x661d9ea4F9B2d395436BBB601a8856Bcd0B8dA57"
-      }
+      "ENSRegistry": "0xb32Feb0530C73FEc90BB985fA40894196938a9f6",
+      "BaseRegistrarImplementation": "0x42E43F785A42bB1696c0a1A036cDe8c7d00d4f4C",
+      "ReverseRegistrar": "0x85FBec85C99b3A8d236e5c64e861f7E62Cdf2De8",
+      "StaticMetadataService": "0xADA2302c6F40269d6DcCd038F8b8B3c2FAe60dd3",
+      "NameWrapper": "0xeb20750987C515732AE4741946f6def43a0200Dd",
+      "PublicResolver": "0xB579f4c45e0CC62f2b4b3A75F23D883E6474A26B",
+      "SimplePriceOracle": "0x78b003C0d809F8e3FC57d8f37Ae2918dbc998512",
+      "ETHRegistrarController": "0x35D74fB7Dc2eA96730Eec42CDe06585D3b17D4c3",
+      "poseidon-solidity/PoseidonT3.sol:PoseidonT3": "0xF58959e3F862557397122cc2809df276b4C83352",
+      "SemaphoreVerifier": "0x6C42599435B82121794D835263C846384869502d",
+      "SignatureVerifier": "0x636657410AFb97bfB284106a06Fe3Fe274cAc371",
+      "GlobalRegistrarController": "0x941e2d34043511e433949Cf748d94Dc4711b23e3",
+      "ProvisionRegistry": "0x0623CD460Bcf07A94f1b54Ea203f014Ed1121E2f",
+      "SecondLevelInteractor": "0x6b07600D77237Ffd9158F6af290010Fc0ff443F0",
+      "ArnaconResolver": "0x752c3428e8443A0e8F0fC10194b1076c98Bc54b4",
+      "SecondLevelController": "0xA679f8eD9eF0Ca064a8560C7c561cD33791AF2f8",
+      "ProductsNFT": "0x1A971D3581a7e546928960337980623bA127Af61"
+    }
 };
 
 module.exports = { amoy };
