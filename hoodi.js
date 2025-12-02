49a248b9c04ad079eab843a97c070da5d0380bc9
Matan-Fridman <matan@cellact.com>
2025-12-02 11:41:58 +0100

    added hoodi

---
+++ hoodi.js
@@ -0,0 +1,30 @@
+const hoodi = {
+    rpcUrl: 'https://rpc.hoodi.ethpandaops.io',
+    contractAddresses: {
+        "ENSRegistry": "0x5841d17010252BE760D055cba2f2853874457443",
+        "BaseRegistrarImplementation": "0x52b02476091Dd8FeFe3FdfB33015F384Ada533D5",
+        "ReverseRegistrar": "0x67D7C0f6f19A7FA8D22AE1f3A22f34C723BC1b73",
+        "StaticMetadataService": "0xDa634A7630DB2e9E6d5A16e9932cE6beF193691F",
+        "NameWrapper": "0x0140420b3e02b1A7d4645cE330337bc8742C3Df5",
+        "SimplePriceOracle": "0xB612Fe5ed5a203F5B7A0dF215712CB792e140dE9",
+        "ETHRegistrarController": "0x3D1A496C00510E0459F14ddc4c855CE8682dF37B",
+        "PublicResolver": "0x9427fF61d53deDB42102d84E0EC2927F910eF8f2",
+        "SignatureVerifierProvider": "0xE89ec4eAB13E1F49933185A43c47f66dc79Bd2a1",
+        "GlobalRegistrarController": "0xA5b27355cFb9ce8aCD2804C5b47C6F1316f33d6e",
+        "ProvisionRegistry": "0x6e0A65396233B8A29d76cA2DDd87bc5F19A82c36",
+        "SignatureVerifier": "0xcF3e8Cd243eEeB43f0559A514c17fab43C9a2aE2",
+        "PALOToken": "0xf4019d9245F4f83364A4c7F4525d79f53F2E7CF2",
+        "ServiceProviderRegistry": "0x93Fa15d969cbb6F720FEA64861a94eE0E7D58B94",
+        "SecondLevelInteractor": "0x151ed065b04583ABB0Ceb21d37C266e41AA3c7E8",
+        "ArnaconResolver": "0xc06E821da811b0735DA5493F1732a25EB7005412",
+        "SecondLevelController": "0x6eB4C6Cf097e030F105283042cBC2457f62e8413",
+        "ProductsNFT": "0xDB157e44A4eCe27Be1644bcDfb648f0f24081565",
+        "InteractorFactory": "0x2DFa945c0A33FE4f169977aC3863a1e0BbEbAC58",
+        "ResolverFactory": "0xCCeeb395d17FE84304C58e30ba1043EdE60c7f88",
+        "NFTFactory": "0x5Fc44e003AfE041B31f1207791cf3367e598BD05",
+        "RegistryFactory": "0xd6Ad3Fab215aEEdD8f6fffA89a95672d659F2DEE",
+        "DomainDelegation": "0x90281C58cc26909686c123d1d8031D25e82daAac"
+      }
+};
+
+module.exports = { hoodi };
--- networks.js
+++ networks.js
@@ -1,12 +1,13 @@
 const { amoy } = require('./amoy');
 const { polygon } = require('./polygon');
 const { sapphireTestnet } = require('./sapphire-testnet');
+const { hoodi } = require('./hoodi');
 
 const networkToChainId = {
     'polygon': 137,
     'amoy': 80002,
     'sepolia': 11155111,
-    'hoodi': 11155420,
+    'hoodi': 560048,
     'tara': 841,
     'sapphire': 23295
 };
@@ -17,7 +18,8 @@ const chainIdToNetwork = {
     11155111: null, // sepolia - placeholder for future implementation
     11155420: null, // hoodi - placeholder for future implementation
     841: null,      // tara - placeholder for future implementation
-    23295: sapphireTestnet     // sapphire - placeholder for future implementation
+    23295: sapphireTestnet,     // sapphire - placeholder for future implementation
+    560048: hoodi
 };
 
 /**
